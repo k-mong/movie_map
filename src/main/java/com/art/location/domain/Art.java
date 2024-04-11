00ce0eec17eae7c86b2e65289b2a5106f7d4e8a6
k-mong <rlaghdwns777@naver.com>
2024-04-11 20:49:01 +0900

    Feat:Entity 추가

--- src/main/java/com/art/location/domain/Art.java
+++ src/main/java/com/art/location/domain/Art.java
@@ -4,16 +4,22 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Art {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String Act_name;
-    private String Sex;
+    private String name;    // 작품명
+    private String year;    // 년도
 }
