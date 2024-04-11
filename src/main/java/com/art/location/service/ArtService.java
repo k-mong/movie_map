68ac3761c603416228ef0b0e8801291210af8b24
k-mong <rlaghdwns777@naver.com>
2024-04-12 02:00:17 +0900

    Feat:ArtRepository

---
+++ src/main/java/com/art/location/service/ArtService.java
@@ -0,0 +1,10 @@
+package com.art.location.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ArtService {
+
+}
